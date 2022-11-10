81ad9abf24154443b1a772c7bb328e3ff62e1bcf
Jana <jana.samson@hotmail.fr>
2022-11-09 22:13:32 -0500

    translation of projects section

---
+++ data/projects.js
@@ -0,0 +1,68 @@
+const projects = [
+    {
+    "fr":
+        [
+            {
+                "title": "E-commerce",
+                "description": "App e-commerce pour une compagnie de système audio développé avec React.",
+                "linkToDemo": "https://aesthetic-centaur-7df753.netlify.app/",
+                "linkToCode": "https://github.com/jajasam/Ecommerce-app",
+                "preview": "./projects-preview/Ecomm.png"
+            },
+            {
+                "title": "Liste to do",
+                "description": "Ce projet de Frontend mentor a été une pratique pour répliqué un design au pixel prêt. Cette liste to do offre deux modes (light et dark) et permet de réorganiser les to do en les glissant et déposant.",
+                "linkToDemo": "https://jajasam.github.io/To-do-app/",
+                "linkToCode": "https://github.com/jajasam/To-do-app",
+                "preview": "./projects-preview/Todo.png"
+            },
+            {
+                "title": "Weather App",
+                "description": "API",
+                "linkToDemo": "",
+                "linkToCode": "",
+                "preview": ""
+            },
+            {
+                "title": "This portfolio",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque magni temporibus.",
+                "linkToDemo": "janasamson.com",
+                "linkToCode": "",
+                "preview": "./projects-preview/Portfolio.jpeg"
+            }
+        ],
+    "en":
+        [
+            {
+                "title": "E-commerce",
+                "description": "Ecommerce app for audio systems built with React.",
+                "linkToDemo": "https://aesthetic-centaur-7df753.netlify.app/",
+                "linkToCode": "https://github.com/jajasam/Ecommerce-app",
+                "preview": "./projects-preview/Ecomm.png"
+            },
+            {
+                "title": "To do list",
+                "description": "This project from Frontend mentor was a practice to replicate pixel perfect a given design. This todo list is responsive, offers two modes (light and dark) and contains a drag and drop functionnality to reorder the todos.",
+                "linkToDemo": "https://jajasam.github.io/To-do-app/",
+                "linkToCode": "https://github.com/jajasam/To-do-app",
+                "preview": "./projects-preview/Todo.png"
+            },
+            {
+                "title": "Weather App",
+                "description": "API",
+                "linkToDemo": "",
+                "linkToCode": "",
+                "preview": ""
+            },
+            {
+                "title": "This portfolio",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque magni temporibus.",
+                "linkToDemo": "janasamson.com",
+                "linkToCode": "",
+                "preview": "./projects-preview/Portfolio.jpeg"
+            }
+        ]
+    }
+]
+
+export { projects };
--- script.js
+++ script.js
@@ -1,71 +1,50 @@
+import { projects } from './data/projects.js'
+
 const skillsContainer = document.querySelector(".skills_container");
 const projectsContainer = document.querySelector(".projects_container");
 const langBtn = document.querySelector(".lang-btn");
 
 
-let language;
+let language = 'fr';
+const projectsInCurrentLang = projects[0][language];
 
 const skills = ["HTML","CSS", "JavaScript", "React", "Bootstrap", "VSC", "Git", "Figma"];
-const projects = [
-    {
-        title: "E-commerce",
-        description: "Ecommerce app for audio systems built with React.",
-        linkToDemo: "https://aesthetic-centaur-7df753.netlify.app/",
-        linkToCode: "https://github.com/jajasam/Ecommerce-app",
-        preview: "./projects-preview/Ecomm.png"
-    },
-    {
-        title: "To do list",
-        description: "This project from Frontend mentor was a practice to replicate pixel perfect a given design. This todo list is responsive, offers two modes (light and dark) and contains a drag and drop functionnality to reorder the todos.",
-        linkToDemo: "https://jajasam.github.io/To-do-app/",
-        linkToCode: "https://github.com/jajasam/To-do-app",
-        preview: "./projects-preview/Todo.png"
-    },
-    {
-        title: "Weather App",
-        description: "API",
-        linkToDemo: "",
-        linkToCode: "",
-        preview: ""
-    },
-    {
-        title: "This portfolio",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque magni temporibus.",
-        linkToDemo: "janasamson.com",
-        linkToCode: "",
-        preview: "./projects-preview/Portfolio.jpeg"
-    }
-]
 
 function displaySkills () {
     let markup = "";
-    
+
     for (let i = 0; i < skills.length; i++) {
         markup += `
             <div class="skill">
-                <img src="./logos/${skills[i]}.png" alt="${skills[i]}">
+                <img src="./logos/skills/${skills[i]}.png" alt="${skills[i]}">
                 <h4>${skills[i]}</h4>
             </div>`
     }
-    console.log(skillsContainer)
     skillsContainer.innerHTML = markup;
 };
 
 function displayProjects () {
-    let markup = "";
+    let markup = projectsContainer.innerHTML;
 
-    for (let i = 0; i < projects.length; i++) {
-        const {title, description, linkToDemo, linkToCode, preview} = projects[i];
+    for (let i = 0; i < projectsInCurrentLang.length; i++) {
+        const {title, description, linkToDemo, linkToCode, preview, comingSoon} = projectsInCurrentLang[i];
 
         markup +=  `
-        <div class="project">
+        <div class="project ${comingSoon ? "coming-soon" : ""}">
             <img src="${preview}" class="project-preview" />
+            ${
+                comingSoon ?
+                "<span class='ribbon'>Coming Soon</span>" : ""
+            }
             <div class="project-infos">
                 <h4>${title}</h4>
                 <p>${description}</p>
-                <div class="btns">
-                    <a target="_blank" href="${linkToDemo}" class="btn demo_btn">Demo</a>
-                    <a target="_blank" href="${linkToCode}" class="btn code_btn">Code</a>
+                <div class="project_btns">
+                ${
+                    !comingSoon ?
+                        `<a target="_blank" href="${linkToDemo}" class="btn demo_btn">Demo</a>
+                        <a target="_blank" href="${linkToCode}" class="btn code_btn">Code</a>` : ""
+                }
                 </div>
             </div> 
         </div>`
