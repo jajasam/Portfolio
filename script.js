73851fafa5615f2ca9581e6cd0552816331cb289
Jana <jana.samson@hotmail.fr>
2022-11-11 13:11:59 -0500

    hide menu if nav links are clicked

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerMenuClose = document.querySelector(".hamburger-menu-close");
 const menuModal = document.querySelector(".menu-modal");
 const body = document.querySelector("body");
-
+const navLinksMobile = document.querySelectorAll(".menu-modal .nav-links li");
 
 
 let language = 'en';
@@ -75,3 +75,8 @@ hamburgerMenuClose.addEventListener("click", () => {
     menuModal.classList.add("hidden");
     body.style.overflow = "scroll";
 })
+
+navLinksMobile.forEach(link => link.addEventListener("click", () => {
+    menuModal.classList.add("hidden");
+    body.style.overflow = "scroll";
+}))
