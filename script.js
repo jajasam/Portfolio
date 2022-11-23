c8b14371334c0157d42430cbf8a7d6e90c99d599
Jana <jana.samson@hotmail.fr>
2022-11-22 19:33:24 -0500

    remove duplicate socials content

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ import './scripts/scrollingAnimations.js';
 import './scripts/scrollingEvents.js';
 import './scripts/skills.js';
 import './scripts/menu.js';
+import './scripts/socials.js';
 import './scripts/lang.js';
---
+++ scripts/socials.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.socials').forEach(elem => elem.innerHTML = `
+<ul>
+    <li class="linkedin"> 
+        <a target="_blank" href="https://www.linkedin.com/in/jana-samson/">
+            <div class="icon"></div>
+        </a>
+    </li>
+    <li class="github">
+        <a target="_blank" href="https://github.com/jajasam">
+            <div class="icon"></div>
+        </a>
+    </li>
+    <li class="mail">
+        <a target="_blank" href="mailto:jana.samson@hotmail.fr">
+            <div class="icon"></div>
+        </a>
+    </li>
+</ul>
+`)
