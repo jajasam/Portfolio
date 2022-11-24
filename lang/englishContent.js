118667447bb9f01677f9b2618b01324c89409161
Jana <jana.samson@hotmail.fr>
2022-11-24 10:07:47 -0500

    add color contrast

--- lang/englishContent.js
+++ lang/englishContent.js
@@ -7,7 +7,7 @@ export const englishContent =
                 "role": "Front-End Web Developer",
                 "contact-me": "Contact Me",
                 "resume": "Resume",
-                "about-text": "Hello, I'm Jana! I'm 22 years old and I'm a self-taught developer from Montreal. I have been learning web development for over a year through online resources such as FreeCodeCamp, The Odin Project and Codecademy. I spent a lot of time practicing by building different projects. I was also able to work on real projects on a team during my internship in an agency. This experience confirmed that I wanted to stay in web development and I'm now looking for the next step. My current goal is to level up my skill set as I deliver business value. I am a friendly, hard-working and passionate person. I attach grand importance to details and responsive design.",
+                "about-text": "Hello, I'm Jana! I'm a 22 years old self-taught developer from Montreal. I have been learning web development for over a year through online resources such as FreeCodeCamp, The Odin Project and CodeCademy. I spent a lot of time practicing by building different projects. I was also able to work on real projects on a team during my internship in an agency. This experience confirmed that I wanted to stay in web development and I'm now looking for the next step. My current goal is to level up my skill set as I deliver business value. I am a friendly, hard-working and passionate person. I attach grand importance to details and responsive design.",
                 "coming-soon": "Coming soon",
                 "get-in-touch": "Let's get in touch",
                 "info": "2022 - By Jana Samson"
