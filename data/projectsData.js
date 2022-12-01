602a77889e04a06add07dd05f4b52dbf0b0f81e6
Jana <jana.samson@hotmail.fr>
2022-11-30 23:31:46 -0500

    replace avif files by png and make project description smaller

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,7 +8,7 @@ const projects = [
                 "description": "App e-commerce pour une compagnie de système audio développé.",
                 "linkToDemo": "https://aesthetic-centaur-7df753.netlify.app/",
                 "linkToCode": "https://github.com/jajasam/Ecommerce-app",
-                "preview": "./projects-preview/Ecomm.avif"
+                "preview": "./projects-preview/Ecomm.png"
             },
             {
                 "title": "Liste to do",
@@ -16,7 +16,7 @@ const projects = [
                 "description": "Ce projet de Frontend mentor a été une pratique pour répliqué un design au pixel prêt. Cette liste to do offre deux modes (light et dark) et permet de réorganiser les to do en les glissant et déposant.",
                 "linkToDemo": "https://jajasam.github.io/To-do-app/",
                 "linkToCode": "https://github.com/jajasam/To-do-app",
-                "preview": "./projects-preview/Todo.avif"
+                "preview": "./projects-preview/Todo.png"
             },
             {
                 "title": "Weather App",
@@ -24,7 +24,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "#",
                 "linkToCode": "#",
-                "preview": "./projects-preview/W.avif"
+                "preview": "./projects-preview/W.png"
             },
             {
                 "title": "This portfolio",
@@ -32,7 +32,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "janasamson.com",
                 "linkToCode": "#",
-                "preview": "./projects-preview/Portfolio.avif"
+                "preview": "./projects-preview/Portfolio.png"
             }
         ],
     "en":
@@ -51,7 +51,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "#",
                 "linkToCode": "https://github.com/jajasam/Ecommerce-app",
-                "preview": "./projects-preview/Ecomm.avif"
+                "preview": "./projects-preview/Ecomm.png"
             },
             {
                 "title": "To do list",
@@ -59,7 +59,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "https://jajasam.github.io/To-do-app/",
                 "linkToCode": "https://github.com/jajasam/To-do-app",
-                "preview": "./projects-preview/Todo.avif"
+                "preview": "./projects-preview/Todo.png"
             },
             {
                 "title": "This portfolio",
@@ -67,7 +67,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "janasamson.com",
                 "linkToCode": "https://github.com/jajasam/Portfolio",
-                "preview": "./projects-preview/Portfolio.avif"
+                "preview": "./projects-preview/Portfolio.png"
             },
             {
                 "title": "Weather App",
@@ -75,7 +75,7 @@ const projects = [
                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
                 "linkToDemo": "#",
                 "linkToCode": "#",
-                "preview": "./projects-preview/W.avif",
+                "preview": "./projects-preview/W.png",
                 "comingSoon": true
             },
         ]
