947fe2f13ebad4795cde0762981eda1954efd2e7
Jana <jana.samson@hotmail.fr>
2022-12-10 19:10:45 -0500

    perf: convert png file to webp

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,28 +4,28 @@ const projects = [
                 "tags": ["HTML", "CSS", "REACT", "SASS", "FIGMA"],
                 "linkToDemo": "https://chic-palmier-e81f0f.netlify.app/",
                 "linkToCode": "https://github.com/jajasam/Designo",
-                "preview": "./projects-preview/Designo.png"
+                "preview": "./projects-preview/Designo.webp"
             },
             {
                 "title": "Audiophile",
                 "tags": ["HTML", "CSS", "REACT", "FIGMA"],
                 "linkToDemo": "https://luxury-moonbeam-ff97f7.netlify.app",
                 "linkToCode": "https://github.com/jajasam/Ecommerce-app",
-                "preview": "./projects-preview/Ecomm.png"
+                "preview": "./projects-preview/Ecomm.webp"
             },
             {
                 "title": "ToDo",
                 "tags": ["HTML", "CSS", "JAVASCRIPT", "FIGMA"],
                 "linkToDemo": "https://jajasam.github.io/To-do-app/",
                 "linkToCode": "https://github.com/jajasam/To-do-app",
-                "preview": "./projects-preview/Todo.png"
+                "preview": "./projects-preview/Todo.webp"
             },
             {
                 "title": "Portfolio",
                 "tags": ["HTML", "CSS", "JAVASCRIPT"],
                 "linkToDemo": "janasamson.com",
                 "linkToCode": "https://github.com/jajasam/Portfolio",
-                "preview": "./projects-preview/Portfolio.png"
+                "preview": "./projects-preview/Portfolio.webp"
             }
         ]
 
